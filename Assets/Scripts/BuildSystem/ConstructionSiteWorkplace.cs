d92536e18172255971b8bc1bd665efeaba5c09b1
00wz <130168402+00wz@users.noreply.github.com>
2024-02-09 18:44:25 +0300

    add build logic to ConstructionSiteWorkplace

--- Assets/Scripts/BuildSystem/ConstructionSiteWorkplace.cs
+++ Assets/Scripts/BuildSystem/ConstructionSiteWorkplace.cs
@@ -1,13 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public class ConstructionSiteWorkplace : Workplace
 {
+    [SerializeField]
+    private float BuildTime = 3f;
+
     private GameObject _buildingPrefab;
+    private int _numberOfBuilders = 0;
+    private CompositeDisposable _buildEveryFrameSubscription = new();
 
     public void Init(GameObject BuildingPrefab)
     {
         _buildingPrefab = BuildingPrefab;
+    }
+
+    public override void StartWork()
+    {
+        _numberOfBuilders++;
+        if(_buildEveryFrameSubscription.Count == 0)
+        {
+            Observable.EveryUpdate().Subscribe(_ => Build()).AddTo(_buildEveryFrameSubscription);
+        }
+    }
+
+    public override void EndWork()
+    {
+        _numberOfBuilders--;
+        if(_numberOfBuilders <= 0)
+        {
+            _buildEveryFrameSubscription.Clear();
+        }
+    }
+
+    private void Build()
+    {
+        BuildTime -= (_numberOfBuilders * Time.deltaTime);
+        if (BuildTime <= 0)
+        {
+            InstantiateBuilding();
+        }
+    }
+
+    private void InstantiateBuilding()
+    {
+        Instantiate(_buildingPrefab, transform.position, transform.rotation, transform.parent);
+        Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        _buildEveryFrameSubscription.Clear();
     }
 }
